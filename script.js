c5e1fd511c33deaaad34002799de943d01736225
rosnbro <benro95@gmail.com>
2022-11-15 12:46:28 -0500

    clean up logic for adding validation messages, add style for invalid messages, add placeholders and regex for inputs

--- script.js
+++ script.js
@@ -17,6 +17,12 @@ phoneNum.addEventListener('blur', validate);
 email.addEventListener('blur', validate);
 password.addEventListener('blur', validate);
 confirmPass.addEventListener('blur', matchPassword);
+firstName.addEventListener('focus', tryInput);
+lastName.addEventListener('focus', tryInput);
+phoneNum.addEventListener('focus', tryInput);
+email.addEventListener('focus', tryInput);
+password.addEventListener('focus', tryInput);
+confirmPass.addEventListener('focus', tryInput);
 
 // Functions
 
@@ -31,33 +37,64 @@ function validateAll() {
 
 function validate(e) {
   let targetElement;
-  if (e.target) {
-     targetElement = e.target.id;
-  } else targetElement = e;
-  
+  let message = 'Please enter a valid input.';
+
+  if (!e.target) {
+    targetElement = document.getElementById(e);
+  } else targetElement = e.target;
+
   switch (targetElement) {
     case 'firstName':
-
+      if (targetElement.validity.patternMismatch) {
+        message = 'Please enter a valid name';
+      } else if (targetElement.validity.tooLong) {
+        message = 'Please enter a shorter name';
+      }
       break;
     case 'lastName':
-
+      if (targetElement.validity.patternMismatch) {
+        message = 'Please enter a valid name';
+      } else if (targetElement.validity.tooLong) {
+        message = 'Please enter a shorter name';
+      }
       break;
     case 'phoneNum':
-
+      if (targetElement.validity.patternMismatch) {
+        message = 'Please enter a valid phone number';
+      }
       break;
     case 'email':
-
+      if (targetElement.validity.patternMismatch) {
+        message = 'Please enter a valid email address';
+      }
       break;
     case 'password':
 
       break;
   }
+
+  if (targetElement.validity.valid) {
+    document.getElementById(targetElement.id + 'Error').textContent = '';
+  } else {
+    document.getElementById(targetElement.id + 'Error').textContent = message;
+  }
+  
 }
 
 function matchPassword() {
   if (password.value === confirmPass.value) {
-    console.log('ayy they match');
+    document.getElementById(password.id + 'Error').textContent = '';
+    document.getElementById(confirmPass.id + 'Error').textContent = '';
   } else {
-    console.log('boooo');
+    document.getElementById(password.id + 'Error').textContent = 'passwords must match.';
+    document.getElementById(confirmPass.id + 'Error').textContent = 'passwords must match.';
+  }
+}
+
+function tryInput(e) {
+  document.getElementById(e.target.id + 'Error').textContent = '';
+  if (e.target.id === 'confirmPass' || e.target.id === 'password') {
+    document.getElementById('passwordError').textContent = '';
+    document.getElementById('confirmPassError').textContent = '';
   }
 }
